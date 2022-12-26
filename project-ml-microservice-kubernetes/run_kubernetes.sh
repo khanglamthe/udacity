7637aa1b44077985dc75ec8e71ad38c4c4604c1f
khanglamthe <khanglamthe@gmail.com>
2022-12-26 09:11:32 +0000

    commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=khanglt/udacity:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- create deployment --image=$dockerpath khanglt-udacity-app
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
+podname=$(minikube kubectl -- get pods -l app=khanglt-udacity-app -o jsonpath={.items[0].metadata.name})
+minikube kubectl -- expose deployment khanglt-udacity-app --port=80 --name=khanglt-udacity-http-service
+minikube kubectl -- port-forward $podname 8089:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,11 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=khanglt/udacity:v1.0.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
